5580faafe5753e69d82d657e4d593c39fe2e032c
asnhck12 <ahussain93@live.co.uk>
2024-08-14 15:30:21 +0100

    Update PersonalDisplay.jsx

--- src/components/PersonalDisplay.jsx
+++ src/components/PersonalDisplay.jsx
@@ -1,6 +1,3 @@
-// import { useState } from "react";
-// import Personal from "./Personal";
-
 function PersonalDisplay({fName,
     // sName,
     email,
@@ -10,9 +7,7 @@ function PersonalDisplay({fName,
     return (
         
         <div className="personalDetails">
-        {/* <h2> Personal Details</h2> */}
                 <h1> {fName}</h1>
-                {/* <p> {sName}</p> */}
                 <div className="contactDetails">
                 <p> {email}</p>
                 <p> {number}</p>
